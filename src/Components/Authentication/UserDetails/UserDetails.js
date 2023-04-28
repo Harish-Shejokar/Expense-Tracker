7c10c14f11d30fccb53207de3f70fabe780b12bc
Harish-Shejokar <90904311+Harish-Shejokar@users.noreply.github.com>
2023-04-28 16:53:56 +0530

    get userDAta from fireBase

--- src/Components/Authentication/UserDetails/UserDetails.js
+++ src/Components/Authentication/UserDetails/UserDetails.js
@@ -1,49 +1,88 @@
-import React,{useRef} from 'react'
-import { Button, Container,Row,Col } from 'react-bootstrap'
+import React, { useRef, useEffect,useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 const UserDetails = () => {
-    const nameRef = useRef();
+    
+  const nameRef = useRef();
     const photoRef = useRef();
 
-    const updateUserDetailsOnFireBase = async(name,photo) => {
-         try {
-           const response = await fetch(
-             "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBnUt6SmiCjCExXs2Pb4ir_uwH5us-ho2w",
-             {
-               method: "POST",
-               body: JSON.stringify({
-                 idToken: localStorage.getItem("token"),
-                 displayName: name,
-                 photoUrl: photo,
-                //  deleteAttribute: ["abcd", "abcd.jpg"],
-                 returnSecureToken : true,
-               }),
-               headers: {
-                 "Content-Type": "application/json",
-               },
-             }
-           );
-           if (response.ok) {
-             console.log("update OK");
-               const data = await response.json();
-               console.log(data);
-             
-           } else {
-             console.log("update not OK");
-            //  alert("Invalid Authentication");
-           }
-         } catch (err) {
-           console.log(err);
-         }
-    }
+    let [oldName,setName]=useState("");
+    let [oldPhoto,setPhoto]=useState("");
 
-    const changePasswordHandler = () => {
-        const name = nameRef.current.value;
-        const photo = photoRef.current.value;
-        console.log(name, photo);
+  const getUserDataFromFireBase = async (token) => {
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBnUt6SmiCjCExXs2Pb4ir_uwH5us-ho2w",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: token,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-        updateUserDetailsOnFireBase(name, photo);
+      if (response.ok) {
+        console.log("get User Data OK");
+        const data = await response.json();
+          console.log(data.users[0]);
+          setName((prev) => data.users[0].displayName);
+          setPhoto((prev) => data.users[0].photoUrl);
+          console.log(oldName,oldPhoto)
+          
+      } else {
+        console.log("signUp not OK");
+        alert("Invalid Authentication");
+      }
+    } catch (err) {
+      console.log(err);
     }
+  };
+
+  useEffect(() => {
+    getUserDataFromFireBase(localStorage.getItem("token"));
+  }, []);
+
+  const updateUserDetailsOnFireBase = async (name, photo) => {
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBnUt6SmiCjCExXs2Pb4ir_uwH5us-ho2w",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: localStorage.getItem("token"),
+            displayName: name,
+            photoUrl: photo,
+            //  deleteAttribute: ["abcd", "abcd.jpg"],
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        console.log("update OK");
+        const data = await response.json();
+        console.log(data);
+      } else {
+        console.log("update not OK");
+        //  alert("Invalid Authentication");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const changeDetailsHandler = () => {
+    const name = nameRef.current.value;
+    const photo = photoRef.current.value;
+    console.log(name, photo);
+
+    updateUserDetailsOnFireBase(name, photo);
+  };
 
   return (
     <div>
@@ -55,15 +94,27 @@ const UserDetails = () => {
         <Row>
           <Col className="mt-3">
             <label className="lg-12 mx-2 ">Full Name</label>
-            <input className="lg-12 mx-5" type="text" ref={nameRef} />
+            <input
+              value={oldName}
+              className="lg-12 mx-5"
+              type="text"
+              ref={nameRef}
+              onChange={e => setName(e.target.value)}
+            />
 
             <label className="lg-12 mx-2">Profile URL</label>
-            <input className="lg-12 mx-5" type="url" ref={photoRef}/>
+            <input
+              value={oldPhoto}
+              className="lg-12 mx-5"
+              type="url"
+              ref={photoRef}
+              onChange={e => setPhoto(e.target.value)}
+            />
           </Col>
         </Row>
         <Row>
           <Col className="mt-2">
-            <Button onClick={changePasswordHandler} variant="success">
+            <Button onClick={changeDetailsHandler} variant="success">
               Update
             </Button>
           </Col>
@@ -71,6 +122,6 @@ const UserDetails = () => {
       </Container>
     </div>
   );
-}
+};
 
-export default UserDetails
+export default UserDetails;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import {Navbar,Nav,Container} from 'react-bootstrap';
+import { Navbar, Nav, Container, } from 'react-bootstrap';
+import { Link, } from 'react-router-dom';
+  
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">Expense-Tracker</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/home" style={{color:"white",textDecoration:"none"}}>Expense-Tracker</Link>
+        </Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#features">Products</Nav.Link>
